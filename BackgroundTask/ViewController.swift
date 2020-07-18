079dfd68aa04ec62c4fa98437f3fdb91cd9705fa
王伟 <steve.wangw@goertek.com>
2020-07-18 14:50:46 +0800

    Initial Commit

---
+++ BackgroundTask/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BackgroundTask
+//
+//  Created by Steve_Mac on 2020/7/18.
+//  Copyright © 2020 goertek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
