833ba7027d1760cd0a5b4fd532dafcd47dc77e6b
王伟 <steve.wangw@goertek.com>
2020-07-23 14:21:29 +0800

    initial

--- BackgroundTask/AppDelegate.swift
+++ BackgroundTask/AppDelegate.swift
@@ -7,30 +7,114 @@
 //
 
 import UIKit
+import BackgroundTasks
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+    var window: UIWindow?
 
+    // 在info.plist中定义的ID
+    let kRefreshTaskId = "com.chadai.com.test.backgroundmodel.fetch"
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        if #available(iOS 13.0, *) {
+            
+            let isRegistered = BGTaskScheduler.shared.register(forTaskWithIdentifier: kRefreshTaskId, using: nil) { (bgTask) in
+                //
+                self.handleRefreshAction(task: bgTask)
+            }
+            
+            if isRegistered {
+                print("BGTaskScheduler is registered!")
+            }else {
+                print("BGTaskScheduler register failed!")
+            }
+            
+        }else {
+            // < 13.0 step 1
+            application.setMinimumBackgroundFetchInterval(20 * 60)
+            
+        }
+        
+        let filePath = NSHomeDirectory() + "/Documents" + "/QiLog.txt"
+        print(filePath)
+
         return true
     }
-
-    // MARK: UISceneSession Lifecycle
-
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    
+    
+    // < 13.0 step 2
+    func application(_ application: UIApplication, performFetchWithCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
+        
+        // 执行唤醒操作
+        // ...
+        fetchAction(fetchInfo: #function)
+        completionHandler(.newData)
+        
     }
-
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    
+    @available(iOS 13.0, *)
+    func handleRefreshAction(task:BGTask) {
+        
+        scheduleAppRefreshAction()
+        
+        fetchAction(fetchInfo: #function)
+        
     }
+    
+    @available(iOS 13.0, *)
+    func scheduleAppRefreshAction() {
+        fetchAction(fetchInfo: #function)
+        let request = BGAppRefreshTaskRequest.init(identifier: kRefreshTaskId)
+        request.earliestBeginDate = NSDate.init(timeIntervalSinceNow: 15 * 60) as Date
+        do {
+            try BGTaskScheduler.shared.submit(request)
+        } catch let err as NSError {
+            print(err)
+        }
+    }
+    
+    func applicationDidEnterBackground(_ application: UIApplication) {
+        if #available(iOS 13.0, *) {
+            scheduleAppRefreshAction()
+        }
+    }
+    
+    // APP唤醒时的执行任务
+        func fetchAction(fetchInfo:String) {
+            
+            print(#function)
+//            let fileU = FileUtil.init()
+//
+//            let date = Date.init()
+//            let format = DateFormatter.init()
+//            format.dateFormat = "yyyy-MMdd-HH:mm:ss"
+//            let dStr = format.string(from: date)  + "  :  " + fetchInfo + "\n\n"
+//
+//    //        fileU.createLogDirectory(direPath: fileU.logPath! + "/" + dStr)
+//            fileU.createLogFile(filePath: fileU.logPath! + "/fetch.txt", fileData: dStr.data(using: .utf8))
+            
+            let date = Date.init()
+            let dateFormatter = DateFormatter.init()
+            dateFormatter.dateFormat = "yyy-MM-dd HH:mm"
+            let timeString = dateFormatter.string(from: date)
+            
+            let filePath = NSHomeDirectory() + "/Documents" + "/QiLog.txt"
+            print(filePath)
+            if !FileManager.default.fileExists(atPath: filePath) {
+                let data = timeString.data(using: .utf8)
+                FileManager.default.createFile(atPath: filePath, contents: data, attributes: nil)
+            }else {
+                var data = NSData.init(contentsOfFile: filePath)
+                let originalContent = String.init(data: data! as Data, encoding: .utf8)
+                let content = originalContent! + String.init(format: "\n 时间  %@ - %@\n", timeString,fetchInfo)
+                data = content.data(using: .utf8) as NSData?
+                data?.write(toFile: filePath, atomically: true)
+            }
+            
+        }
 
 
 }
